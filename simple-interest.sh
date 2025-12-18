fc6b47620120c6f68b34387723002ed367a18675
Olga12IT <obogusch@gmx.de>
2025-12-18 12:19:01 +0100

    simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "=== Zinsrechner ==="
-read -p "Kapital (Euro): "
-read -p "Zinssatz (%): "
-read -p "Zeitraum (Jahre): "
+read p "Kapital (Euro): "
+read p "Zinssatz (%): "
+read p "Zeitraum (Jahre): "
 zinsen=$(echo "$kapital * $zinssatz * $zeitraum / 100" | bc -1)
 echo "Zinsen: $zinsen Euro"
